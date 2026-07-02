5997c018c519947c43859d35f44c9b4af53e64c6
Quoc Khanh <qckhanh205@gmail.com>
2026-07-02 22:32:11 +0700

    add frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import GameDetail from './pages/GameDetail';
 import Cart from './pages/Cart';
-import Navbar from './components/Navbar'; // Import Navbar đã tách
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
     <Router>
       <div className="min-h-screen flex flex-col">
-        {/* Gọi Component Navbar ở đây */}
         <Navbar />
 
-        {/* Các trang hiển thị */}
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
